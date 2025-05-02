26c1b974ca93e067c35ce430e30ba4b17e3e7105
Mehdi Radfar <metiii.py@gmail.com>
2025-05-02 23:02:45 +0330

    Accounts - 👤 First Initialize of the views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from .serializers import (
+    UserSerializer,
+    RegisterSerializer,
+    PasswordChangeSerializer,
+    ActivateAccountSerializer,
+    PasswordResetRequestSerializer,
+    PasswordResetConfirmSerializer
+)
 
+User = get_user_model()
 # Create your views here.
